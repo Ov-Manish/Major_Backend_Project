6440e9abbc3a3f60adbfab670cddde55696dea55
Ov-Manish <manishsharma75085@gmail.com>
2024-08-28 21:43:38 +0530

    User and Video Models are created !!!

---
+++ src/models/user.model.js
@@ -0,0 +1,88 @@
+import mongoose,{Schema} from "mongoose";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new Schema({
+    username :{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase: true,
+        trim : true,
+        index :true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim :true,
+    },
+    fullName:{
+        type:String,
+        required:true,
+        trim :true
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    coverImage:{
+        type:String,
+    },
+    password:{
+        type:String,
+        required:[true,'Password is required']
+    },
+    refreshToken:{
+        type:String,
+    }
+
+},{timestamps:true});
+
+userSchema.pre('save',async function(){
+    if (!this.password.isModified('password')) return next();
+        this.password = bcrypt.hash(this.password,10);
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+// Generate Access Token :
+userSchema.methods.genrateAccessToken = function(){
+    jwt.sign(
+        {
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+        },
+
+        process.env.AACCESS_TOKEN_SECRET,
+
+        {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+// Generate Refresh Token :
+userSchema.methods.generateRefreshToken=function(){
+    jwt.sign(
+        {
+            _id:this._id,
+        },
+
+        process.env.REFRESH_TOKEN_SECRET,
+
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+)
+}
+
+
+
+export const User =mongoose.model('User',userSchema);
